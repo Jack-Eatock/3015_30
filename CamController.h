1d23691c4d8340ddb5abb7eedfff5da1cda3ccf4
Jack Eatock <jack.eatock06@gmail.com>
2024-03-16 19:01:07 +0000

    WIP

--- CamController.h
+++ CamController.h
@@ -9,7 +9,7 @@ class CamController
 {
 public:
 
-	float speed = .4f;
+	float speed = .15f;
 	glm::vec3 position = glm::vec3(6.0f, 7.0f, 7.5f);
 	glm::vec3 Orientation = glm::vec3(-0.6f, -0.5f, -1.0f);
 	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
